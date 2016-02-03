bd463fa5d9c18de59849238b863207010c987b38
Alex Rosenfeld <alexrosenfeld10@gmail.com>
2016-02-03 17:55:06 -0500

    Beginning to add error checking to init. Finished __repr__. Begins methods to add and subtract resources.

--- catan_classes.py
+++ catan_classes.py
@@ -5,15 +5,67 @@
 # Jake Dharmasiri
 # Will Ieong
 
-class Player():
+class Player:
     def __init__(self, name, color):
         self.name = name
-        self.color = color
+        if str.lower(color) in\
+           ["black", "white", "orange", "blue", "red"]:
+            self.color = color
+        else:
+            ## THROW ERROR HERE
+            
         # order: brick, ore, sheep, wheat, wood
-        resources = [0, 0, 0, 0, 0]
+        self.resources = [0, 0, 0, 0, 0]
+        
         # dev_cards = [TBD]
+        
     def __repr__(self):
-        s = "Name: " + name + \
-            "\nColor: " + color +\
+        s = "Name: " + self.name + \
+            "\nColor: " + self.color +\
             "\nCurrent resources:\n"
+        res = "Number of Bricks: " + str(self.resources[0])
+        res += "Number of Ore: " + str(self.resources[1])
+        res += "Number of Sheep: " + str(self.resources[2])
+        res += "Number of Wheat: " + str(self.resources[3])
+        res += "Number of Wood: " + str(self.resources[4])
         return s
+    def add_brick(self):
+        self.resources[0] += 1
+
+    def add_ore(self):
+        self.resources[1] += 1
+
+    def add_sheep(self):
+        self.resources[2] += 1
+
+    def add_wheat(self):
+        self.resources[3] += 1
+
+    def add_wood(self):
+        self.resources[4] += 1
+
+    def subtract_brick(self):
+        self.resources[0] -= 1
+
+    def subtract_ore(self):
+        self.resources[1] -= 1
+
+    def subtract_sheep(self):
+        self.resources[2] -= 1
+
+    def subtract_wheat(self):
+        self.resources[3] -= 1
+
+    def subtract_wood(self):
+        self.resources[4] -= 1
+
+
+
+
+
+
+
+
+
+
+        
