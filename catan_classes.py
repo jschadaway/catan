089429261c41f2014a78786c8fab97985a41d06c
William <williamieong95@gmail.com>
2016-02-03 18:08:37 -0500

    Changed resources into a dictionary

--- catan_classes.py
+++ catan_classes.py
@@ -3,7 +3,7 @@
 ##
 # Alex Rosenfeld
 # Jake Dharmasiri
-# Will Ieong
+# William Ieong
 
 class Player:
     def __init__(self, name, color):
@@ -11,11 +11,13 @@ class Player:
         if str.lower(color) in\
            ["black", "white", "orange", "blue", "red"]:
             self.color = color
-        else:
+        #else:
             ## THROW ERROR HERE
+        
             
         # order: brick, ore, sheep, wheat, wood
-        self.resources = [0, 0, 0, 0, 0]
+        self.resources = {'Bricks': 0, 'Ore': 0, 'Sheep': 0, 'Wheat': 0, 'Wood': 0}
+        
         
         # dev_cards = [TBD]
         
@@ -23,41 +25,46 @@ class Player:
         s = "Name: " + self.name + \
             "\nColor: " + self.color +\
             "\nCurrent resources:\n"
-        res = "Number of Bricks: " + str(self.resources[0])
-        res += "Number of Ore: " + str(self.resources[1])
-        res += "Number of Sheep: " + str(self.resources[2])
-        res += "Number of Wheat: " + str(self.resources[3])
-        res += "Number of Wood: " + str(self.resources[4])
+        res = "Number of Bricks: " + str(self.resources['Bricks'])
+        res += "Number of Ore: " + str(self.resources['Ore'])
+        res += "Number of Sheep: " + str(self.resources['Sheep'])
+        res += "Number of Wheat: " + str(self.resources['Wheat'])
+        res += "Number of Wood: " + str(self.resources['Wood'])
         return s
-    def add_brick(self):
-        self.resources[0] += 1
 
-    def add_ore(self):
-        self.resources[1] += 1
-
-    def add_sheep(self):
-        self.resources[2] += 1
-
-    def add_wheat(self):
-        self.resources[3] += 1
-
-    def add_wood(self):
-        self.resources[4] += 1
-
-    def subtract_brick(self):
-        self.resources[0] -= 1
-
-    def subtract_ore(self):
-        self.resources[1] -= 1
-
-    def subtract_sheep(self):
-        self.resources[2] -= 1
-
-    def subtract_wheat(self):
-        self.resources[3] -= 1
-
-    def subtract_wood(self):
-        self.resources[4] -= 1
+    def add_resource(self, resource):
+        self.resources[resource] += 1
+        
+##    
+##    def add_brick(self):
+##        self.resources[0] += 1
+##
+##    def add_ore(self):
+##        self.resources[1] += 1
+##
+##    def add_sheep(self):
+##        self.resources[2] += 1
+##
+##    def add_wheat(self):
+##        self.resources[3] += 1
+##
+##    def add_wood(self):
+##        self.resources[4] += 1
+##
+##    def subtract_brick(self):
+##        self.resources[0] -= 1
+##
+##    def subtract_ore(self):
+##        self.resources[1] -= 1
+##
+##    def subtract_sheep(self):
+##        self.resources[2] -= 1
+##
+##    def subtract_wheat(self):
+##        self.resources[3] -= 1
+##
+##    def subtract_wood(self):
+##        self.resources[4] -= 1
 
 
 
