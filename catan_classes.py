c2d323942394a761665e2e991148ec12d461fb74
Alex Rosenfeld <alexrosenfeld10@93-53-41-155-wireless1x.bu.edu>
2016-02-03 16:47:53 -0500

    initial commit

---
+++ catan_classes.py
@@ -0,0 +1,16 @@
+#
+# Catan starter code
+##
+# Alex Rosenfeld
+# Jake Dharmasiri
+# Will Ieong
+
+class Player():
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+        # order: brick, ore, sheep, wheat, wood
+        resources = [0, 0, 0, 0, 0]
+        # dev_cards = [TBD]
+
+        
