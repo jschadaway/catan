a4fe61b1f74fe6b816be31c5e0e45b81ba62c525
williamieong <ieong24861@yahoo.com>
2016-02-04 17:53:46 -0500

    Implemented subtraction in the interface class, and added an option to quite the main function. Style edits

--- Catan_Interface.py
+++ Catan_Interface.py
@@ -15,37 +15,58 @@ def run():
     global num_Players
     global player_list
     global resources
+    
     num_Players = int(input("How many players are playing the game? "))
     player_list = [Player() for i in range(num_Players)]
+    
     for i in range(num_Players):
         player_name = input("What is the name of Player " + str(i) + "? ")
         player_color = input("What color is Player " + str(i) + "? ")
         player_list[i].name = player_name
         player_list[i].color = player_color
+        
     while True:
         action = int(input("What would you like to do? "\
                                  + print_major_options()))
         if action == 0:
-
             choice_of_player = int(input("Select a player: " + print_p_options()))
             choice_of_resource = int(input("Select a resource: " + print_r_options()))
             adding = str.lower(input("Would you like to add or subtract " +\
                            resources[choice_of_resource] + "?\n"))
             if adding in ["add", "a", "ad", "addd"]:
                 player_list[choice_of_player].add_resource(resources[choice_of_resource])
+            elif adding in ["subtract", "sub", "s", "subtrcat"]:
+                if player_list[choice_of_player].resources[resources[choice_of_resource]] == 0:
+                    print(player_list[choice_of_player].name + " has none of this resource!")
+                else:
+                    player_list[choice_of_player].subtract_resource(resources[choice_of_resource])
             else:
-                adding = False
+                print("Please type add or subtract")
 
         elif action == 1:
-            ## HERE?
-            x = 4 
+            #print(player_list)
+            #Do we want this to just print the players? Or perhaps take away
+            #Color information
+
+            #Do we want this to display all resources or just for one player?
+            s = ""
+            print(num_Players[1].name)
+            for i in range(num_Players):
+                s += num_Players[i].name + "\n" + \
+                     + num_Players[i].resources + "\n"
+            print(s)
+
+        elif action == 2:
+            print("Game over, thank you for using the Catan Companion App")
+            return
+            
         else:
             print("NEED NEW OPTION!")
 
 def print_major_options():
     s = "\n(0) Edit Resources"
     s += "\n(1) View Resources"
-    s += "\n(2) Other option here?\n"
+    s += "\n(2) End Program\n"
     return s
 
 def print_p_options():
